a7783116a71dbe4eb7d540b8f9ffe8b23e23d7c4
Nadia Jensen <Nadiajjensen98@hotmail.com>
2025-11-14 12:57:37 +0100

    Add project files.

---
+++ WPFAndMVVM1/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFAndMVVM1
+{
+   public class MainViewModel : INotifyPropertyChanged
+    {
+        private string myLabelText = "Text not set yet";
+
+        public string MyLabelText
+        {
+            get { return myLabelText; }
+            set
+            {
+                myLabelText = value;
+                OnPropertyChanged(MyLabelText);
+            }
+        }
+
+        private string myTextBoxText = "Text not set yet";
+        public string MyTextBoxText
+        {
+            get { return myTextBoxText; }
+            set
+            {
+                myTextBoxText = value;
+                OnPropertyChanged(MyTextBoxText);
+            }
+        }   
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler propertyChanged = this.PropertyChanged;
+            if (propertyChanged != null)
+            {
+                propertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+
+    
+}
---
+++ WPFAndMVVM1/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System;   
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFAndMVVM1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       private MainViewModel mvm = new MainViewModel();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = mvm;
+        }
+
+        private void UpdateLabel_Click(object sender, RoutedEventArgs e)
+        {
+            mvm.MyLabelText = DateTime.Now.ToString();  // når du klikker på knappen ”Update Label” i GUI’en, så ændres MyLabelText i din MainViewModel til teksten for den lokale dato og tidspunkt lige nu (DateTime.Now
+        }
+
+    }
+}
